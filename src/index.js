2841e14ec44bec95823fc2659e05e823d4dfd9d5
Omero - Laptop <iiomero2582ii@gmail.com>
2022-12-27 16:50:39 -0500

    at start of learning props

--- src/index.js
+++ src/index.js
@@ -5,11 +5,47 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function formatDate(date) {
+  return date;
+} 
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+            src={props.user.avatarUrl}
+            alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo (props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user ={props.user}/>
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user = {props.author}/>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const sesUrl = 'https://uploads2.yugioh.com/card_images/3500/detail/5385.jpg?1385134497'
+root.render(<Comment author={{name: 'ses', avatarUrl: sesUrl }} date= {new Date().toDateString()} text= 'myComment'/>)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
